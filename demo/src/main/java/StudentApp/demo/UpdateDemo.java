21e9f43c13f633c72b3526a6d9bc0ed0f16742d9
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 09:37:35 +0400

    adding required feilds in UpdateDemo class

--- demo/src/main/java/StudentApp/demo/UpdateDemo.java
+++ demo/src/main/java/StudentApp/demo/UpdateDemo.java
@@ -1,4 +1,37 @@
 package StudentApp.demo;
 
 public class UpdateDemo {
+    private String currentStudentName;
+    private String newStudentName;
+    private boolean status;
+
+    public UpdateDemo(String currentStudentName, String newStudentName, boolean status) {
+        this.currentStudentName = currentStudentName;
+        this.newStudentName = newStudentName;
+        this.status = status;
+    }
+
+    public String getCurrentStudentName() {
+        return currentStudentName;
+    }
+
+    public void setCurrentStudentName(String currentStudentName) {
+        this.currentStudentName = currentStudentName;
+    }
+
+    public String getNewStudentName() {
+        return newStudentName;
+    }
+
+    public void setNewStudentName(String newStudentName) {
+        this.newStudentName = newStudentName;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
