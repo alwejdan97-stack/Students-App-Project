cf3554f1f5557f456c16775037639659c4f98d6e
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 10:29:24 +0400

    add code for updating

--- demo/src/main/java/StudentApp/demo/StudentController.java
+++ demo/src/main/java/StudentApp/demo/StudentController.java
@@ -1,4 +1,23 @@
 package StudentApp.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
 public class StudentController {
+    Student student=new Student("S-01","Wejdan","A+");
+
+    @PutMapping("updateStudent")
+    public Student updateStudent(@RequestParam String studentId){
+        System.out.println("The current student id is: "+student.getStudentId());
+        if(!student.getStudentId().equals(studentId)){
+            student.setStudentId(studentId);
+            System.out.println("Student ID Updated SUCCESSFULLY...");
+        }else{
+            System.out.println("Student ID Is Matching Old ID, Updated FAILD...");
+        }
+        return student;
+    }
 }
