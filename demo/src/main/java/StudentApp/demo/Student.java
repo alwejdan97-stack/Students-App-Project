6915bac90c31633092f85fbbd1ec012341603746
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 09:59:34 +0400

    adding getter and setter methods inside Student class

--- demo/src/main/java/StudentApp/demo/Student.java
+++ demo/src/main/java/StudentApp/demo/Student.java
@@ -10,4 +10,28 @@ public class Student {
         this.studentName = studentName;
         this.grade = grade;
     }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
 }
