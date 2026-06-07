e5167d97352889089a5f9cdc0520e841ad9f1305
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 09:56:12 +0400

    creating UpdateDemoController class

--- demo/src/main/java/StudentApp/demo/DemoApplication.java
+++ demo/src/main/java/StudentApp/demo/DemoApplication.java
@@ -8,6 +8,17 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
+		String currentStudentName="Wejdan";
+		String newStudentName="Hoor";
+		boolean status=false;
 
+		System.out.println("the current name is: "+currentStudentName);
+		if(!currentStudentName.equalsIgnoreCase(newStudentName)){
+			currentStudentName=newStudentName;
+			status=true;
+			System.out.println("Student name UPDATED...");
+		}else{
+			System.out.println("Student name NOT UPDATED...");
+		}
+	}
 }
--- demo/src/main/java/StudentApp/demo/UpdateDemoController.java
+++
@@ -1,4 +0,0 @@
-package StudentApp.demo;
-
-public class UpdateDemoController {
-}
