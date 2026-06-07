2185bf15e32b1c2211cc5a329d7c36f037d2d44a
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-07 09:58:28 +0400

    creating constructor inside Student class

--- demo/src/main/java/StudentApp/demo/Student.java
+++ demo/src/main/java/StudentApp/demo/Student.java
@@ -1,4 +1,13 @@
 package StudentApp.demo;
 
 public class Student {
+    private int studentId;
+    private String studentName;
+    private String grade;
+
+    public Student(int studentId, String studentName, String grade) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.grade = grade;
+    }
 }
